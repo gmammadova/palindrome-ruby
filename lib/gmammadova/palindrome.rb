baae21b62fc0ebb0d83fc9005871e996a3067555
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-05-04 16:03:54 -0400

    Initialized repository

---
+++ lib/gmammadova/palindrome.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "palindrome/version"
+
+module Gmammadova
+  module Palindrome
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/gmammadova/palindrome/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Gmammadova
+  module Palindrome
+    VERSION = "0.1.0"
+  end
+end
