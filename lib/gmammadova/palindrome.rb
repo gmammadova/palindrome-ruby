d35d77d50e1e75285e1c16159d66f5062f74f1ca
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-05-04 21:15:08 -0400

    Finished working and refactored palindrome method

--- lib/gmammadova/palindrome.rb
+++ lib/gmammadova/palindrome.rb
@@ -2,9 +2,17 @@
 
 require_relative "palindrome/version"
 
-module Gmammadova
-  module Palindrome
-    class Error < StandardError; end
-    # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
   end
+  
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
